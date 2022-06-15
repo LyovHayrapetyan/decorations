cfbfb7d7febac85f46b16e82ab032fce415ba21b
LyovHayrapetyan <67322034+LyovHayrapetyan@users.noreply.github.com>
2022-06-15 19:00:47 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function openFunction() {
+    document.getElementById("side-nav").style.left = "0px";
+}
+function closeFunction() {
+    document.getElementById("side-nav").style.left = "-375px";
+}
+function openSearch() {
+    document.getElementById("search-box").style.display = "block";
+}
+function closeSearch() {
+    document.getElementById("search-box").style.display = "none";
+}
